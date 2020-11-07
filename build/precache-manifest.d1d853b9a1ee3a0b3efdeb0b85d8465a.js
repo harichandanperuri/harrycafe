a12bbcc0ab1692bec687ffbddb8951bb01d58038
harry <hachaku999@gmail.com>
2020-11-07 17:04:04 +0530

    initial commit

---
+++ build/precache-manifest.d1d853b9a1ee3a0b3efdeb0b85d8465a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b05e6c3de77d5d50fa710249dbb03c45",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe94c0a4469a7561c5af",
+    "url": "/static/css/2.5c17b9f1.chunk.css"
+  },
+  {
+    "revision": "757b4dce98601864e147",
+    "url": "/static/css/main.4ad16bb5.chunk.css"
+  },
+  {
+    "revision": "fe94c0a4469a7561c5af",
+    "url": "/static/js/2.442b4c21.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.442b4c21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "757b4dce98601864e147",
+    "url": "/static/js/main.74deaf6f.chunk.js"
+  },
+  {
+    "revision": "0980db0307ffad65803b",
+    "url": "/static/js/runtime-main.df5858f9.js"
+  }
+]);
