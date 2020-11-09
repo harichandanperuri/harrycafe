af9348ee26b830604839f14f58352bd3ccfa68d0
harry <hachaku999@gmail.com>
2020-11-09 13:29:58 +0530

    second commit

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.924c9f6348536796b4fc802d0e3599c1.js"
+  "/precache-manifest.11cb718d4214ef95f9b754496b50a70a.js"
 );
 
 self.addEventListener('message', (event) => {
