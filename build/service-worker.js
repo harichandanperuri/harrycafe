a032b05be19f423f593de5add7373c7ce92055ab
harry <hachaku999@gmail.com>
2020-12-13 10:21:17 +0530

    mettle

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ce07f0e0d78c009cf0cb7e732d30cdd5.js"
+  "/precache-manifest.01ebbe8753c6762281cd4f95b24f2726.js"
 );
 
 self.addEventListener('message', (event) => {
